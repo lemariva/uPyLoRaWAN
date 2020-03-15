dc1a4af2ad211cd846929fe6477daa3dbe3ca480
Mauro Riva <mauro.riva@finealyze.com>
2020-03-15 18:38:58 +0100

    test with ESP32-ATOM

--- main.py
+++ main.py
@@ -4,20 +4,21 @@ import urandom
 from sx127x import TTN, SX127x
 from machine import Pin, SPI
 from ttn_config import *
-
+import neopixel
+from letters import characters
 
 __DEBUG__ = True
 
 
 ttn_config = TTN(DEVADDR, NWKEY, APP, country="EU")
 device_pins = {
-    'miso':19,
-    'mosi':27,
-    'ss':18,
-    'sck':5,
-    'dio_0':26,
-    'reset':16,
-    'led':2, 
+    'miso':23,
+    'mosi':19,
+    'ss':22,
+    'sck':33,
+    'dio_0':25,
+    'reset':21,
+    'led':27, 
 }
 
 device_spi = SPI(baudrate = 10000000, 
@@ -27,9 +28,25 @@ device_spi = SPI(baudrate = 10000000,
         miso = Pin(device_pins['miso'], Pin.IN, Pin.PULL_UP))
 
 lora = SX127x(device_spi, pins=device_pins, ttn_config=ttn_config)
-
+np = neopixel.NeoPixel(Pin(27), 25)
 
 frame_counter = 0
+
+text = "SOLIDARITY"
+
+def char_to_leds(letter, color):
+    rgb = color
+    char_matrix = characters.get(letter)
+    led_counter = 0
+    for row in char_matrix:
+        for led in row:
+            if(led):
+                np[led_counter] = rgb
+            else:
+                np[led_counter] = (0, 0, 0)
+            led_counter += 1
+    np.write()
+
 while True:
     timestamp = utime.localtime()
 
@@ -45,5 +62,18 @@ while True:
     
     lora.send_data(data=payload, data_length=len(payload), frame_counter=frame_counter)
     
+    """
+    if lora.received_packet():
+        lora.blink_led()
+        print('something here')
+        payload = lora.read_payload()
+        print(payload)
+    """
+
+    for letter in text:
+        char_to_leds(letter, (20, 20, 20))
+        utime.sleep_ms(600)
+    char_to_leds("!", (50, 5, 0))
+
     frame_counter += 1
-    utime.sleep_ms(10000)
+    utime.sleep_ms(5000)
